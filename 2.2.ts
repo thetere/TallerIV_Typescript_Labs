fa18548318907b415865fa4d45fd8a21145be3a1
thetere <tereseta@gmail.com>
2022-05-24 03:27:04 -0300

    Solución a 2.2.ts

--- 2.2.ts
+++ 2.2.ts
@@ -1,10 +1,25 @@
-class Persona {
-    constructor(public nombre: string, public edad: number) { }
+interface PersonaInterfaz {
+    getNombre(): string;
+    getEdad(): number;
+
+}
+
+class Persona implements PersonaInterfaz {
+    constructor(private nombre: string, private edad: number) { }  
+
+    getNombre(): string {
+        return this.nombre;
+    }
+
+    getEdad(): number {
+        return this.edad;
+    }
+    
 }
 
 const juan = new Persona('Juan', 31);
 
-console.log('[Ejercicio 2.2]', `${juan.nombre} tiene ${juan.edad} anios.`);
+console.log('[Ejercicio 2.2]', `${juan.getNombre()} tiene ${juan.getEdad()} anios.`);
 
 /*
     1 Cree e implemente una interfaz en ‘Persona‘ para asegurarse de que siempre tenga acceso
