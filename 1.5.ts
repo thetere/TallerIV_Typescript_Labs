666b033d1ceb34fd94173223e9bc85a611beb2ef
thetere <tereseta@gmail.com>
2022-05-24 02:21:03 -0300

    Corrección a 1.5.ts

--- 1.5.ts
+++ 1.5.ts
@@ -7,7 +7,7 @@ Dado el siguiente codigo:
 
 const secuencia: number[] = Array.from(Array(10).keys());
 const animales: string[] = ['pinguino', 'oso hormiguero', 'zorro', 'jirafa'];
-const cadenasYNumeros : [number, string][]= [[1, 'uno'], [2, 'dos'], [3, 'tres']];
-const todosMisArreglos = [secuencia, animales, cadenasYNumeros];
+const cadenasYNumeros: [number, string][]= [[1, 'uno'], [2, 'dos'], [3, 'tres']];
+const todosMisArreglos: ((number|string)[]|[number, string][])[] = [secuencia, animales, cadenasYNumeros];
 
 console.log('Ejercicio 1.5', todosMisArreglos);
