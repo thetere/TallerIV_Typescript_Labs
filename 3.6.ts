375a356f75cf6714049aa000e8cab1b88b592d4a
thetere <tereseta@gmail.com>
2022-05-26 19:52:23 -0300

    =3.6.ts

--- 3.6.ts
+++ 3.6.ts
@@ -1,13 +1,15 @@
 let multiply: (val1: number, val2: number) => number;
 let capitalize: (val: string) => string;
 
-multiply = function (value: string): string {
+multiply = function (x: number, y: number): number {
+    return x * y;
+}
+
+capitalize = function (value: string): string {
     return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
 }
 
-capitalize = function (x: number, y: number): number {
-    return x * y;
-}
+
 
 console.log('[Ejercicio 3.6]', capitalize(`habil ${multiply(5, 10)}`));
 
