13e89f1ab45713d1d9af91c8a3939513a29f647b
thetere <tereseta@gmail.com>
2022-05-26 19:45:33 -0300

    +3.5.ts

---
+++ 3.5.ts
@@ -0,0 +1,10 @@
+function layEggs(quantity, color) {
+    console.log(
+        `[Ejercicio 3.5] Acabas de poner ${quantity} huevos ${color}. Buen trabajo!`);
+}
+layEggs();
+
+/*
+1 Añadir anotación de tipo de parámetro
+2 A pesar de que esta función no vuelve, agregue un tipo de retorno explı́cito
+*/
