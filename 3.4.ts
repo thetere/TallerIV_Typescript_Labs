73b3f1ad3a2447e3c9d9dad1026f3f74d5e86769
thetere <tereseta@gmail.com>
2022-05-26 19:39:55 -0300

    =3.4.ts

--- 3.4.ts
+++ 3.4.ts
@@ -1,9 +1,15 @@
-function greet(greeting) {
-    return greeting.toUpperCase();
+function greet(greeting?: string): string {
+    const saludo: string = "hola";
+    /* if (greeting)
+        return greeting.toUpperCase();
+    else
+        return saludo.toUpperCase(); */ 
+    // ^ esta es la clasica, pero en typescript podemos hacerlo rarito,con el ternary operator asi:
+    return (greeting? greeting.toUpperCase() : saludo.toUpperCase());
 }
 
-const defaultGreeting = greet();
-const portugueseGreeting = greet('Oi como vai!');
+const defaultGreeting: string = greet();
+const portugueseGreeting: string = greet('Oi como vai!');
 
 console.log('[Ejercicio 3.4]', defaultGreeting, portugueseGreeting);
 
