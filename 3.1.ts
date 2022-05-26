87590b50bc12eee390f7412a6d5fa154e362c460
thetere <tereseta@gmail.com>
2022-05-25 23:29:20 -0300

    correccion =3.1.ts

--- 3.1.ts
+++ 3.1.ts
@@ -6,7 +6,7 @@ function sumArray(numbers: number[]): number {
     return numbers.reduce(add, 0);
 }
 
-const someSum = sumArray([3, 6, 9]);
+const someSum: number = sumArray([3, 6, 9]);
 
 console.log('[Ejercicio 3.1]', `3 + 6 + 9 === ${someSum}`);
 /*
