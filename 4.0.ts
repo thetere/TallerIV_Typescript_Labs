f5e1b7954f2db78c2e6a9f16b1490ab6c847ffdf
thetere <tereseta@gmail.com>
2022-05-26 20:46:47 -0300

    +&=4.0.ts

---
+++ 4.0.ts
@@ -0,0 +1,30 @@
+/*
+TypeScript es inteligente sobre los posibles tipos de una variable, dependiendo de la ruta del
+código.
+Dado el siguiente codigo:
+*/
+
+function doStuff(value: any): void {
+    if (typeof value === 'string') {
+        console.log(value.toUpperCase().split('').join(' '));
+    } else if (typeof value === 'number') {
+        console.log(value.toPrecision(5));
+    }
+
+    value; // coloque el cursor sobre `valor` aqui
+}
+
+doStuff(2);
+doStuff(22);
+doStuff(222);
+doStuff('hello');
+doStuff(true);
+doStuff({});
+
+console.log('[Ejercicio 4.1]');
+
+/*
+1 Simplemente inspeccione los tipos posibles moviéndose sobre el ’texto’ para ver cómo
+cambia el tipo inferido si se pueden hacer suposiciones de forma segura sobre los tipos
+posibles dentro de la ruta del código dado
+*/
