a592b35c8a5383768fb0760a736dab8fdbc4fdfb
thetere <tereseta@gmail.com>
2022-05-26 22:22:11 -0300

    =4.2.ts

--- 4.2.ts
+++ 4.2.ts
@@ -1,14 +1,18 @@
 function padLeft(value: string, padding: number | string): string {
-    // si padding es un numero, return `${Array(padding + 1).join(' ')}${value}`
-    // si padding es una cadena, return padding + value
+    //return ((typeof padding === 'number')? `${Array(padding + 1).join(' ')}${value}` : padding + value);
+    //esto de arriba funciona pero queda horrible para mi gusto, bastante ilegible, y no chequea q sea string entonces...
+    if (typeof padding === 'number')
+        return `${Array(padding + 1).join(' ')}${value}`
+    else if (typeof padding === 'string')
+        return padding + value
 }
 
 console.log('[Ejercicio 4.2]', `
-    ${padLeft('', 0)}
-    ${padLeft('', '')}
-    ${padLeft('', '')}
-    ${padLeft('', '')}
-    ${padLeft('', '')}
+    ${padLeft('a', 0)}
+    ${padLeft('a', '')}
+    ${padLeft('a', '')}
+    ${padLeft('a', 10)}
+    ${padLeft('a', '   ')}
 `);
 
 /*
