fef97738efbf6e407c5077f8e080983e3bfe3bb6
thetere <tereseta@gmail.com>
2022-06-01 02:53:59 -0300

    +4.4.ts

---
+++ 4.4.ts
@@ -0,0 +1,66 @@
+interface EggLayer {
+    layEggs(): void;
+}
+
+interface Flyer {
+    fly(height: number): void;
+}
+
+interface Swimmer {
+    swim(depth: number): void;
+}
+
+// agregar alias de tipo(s) aqui
+
+class Bird implements BirdLike {
+    constructor(public species: string) { }
+
+    layEggs(): void {
+        console.log('Poniendo huevos de aves.');
+    }
+
+    fly(height: number): void {
+        console.log(`Volando a la altura de ${height} metros.`);
+    }
+}
+
+class Fish implements FishLike {
+    constructor(public species: string) { }
+
+    layEggs(): void {
+        console.log('Poniendo huevos de pescado.');
+    }
+
+    swim(depth: number): void {
+        console.log(`Nadando a una profundidad de ${depth} metros.`);
+    }
+}
+
+function getRandomAnimal() {
+    const animals = [
+        new Bird('puffin'),
+        new Bird('kittiwake'),
+        new Fish('sea robin'),
+        new Fish('leafy seadragon'),
+    ];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+
+function interrogateAnimal(animal = getRandomAnimal()) {
+    animal.swim(10) // se llama solo si es un pez
+    animal.fly(10); // se llama solo si es un pajaro
+
+    return animal.species;
+}
+
+console.log('[Ejercicio 4.4]',
+`Tenemos un ${interrogateAnimal()} en nuestras manos!`);
+
+/*
+1 Las aves y los peces ponen huevos. Sólo los pájaros vuelan. Sólo los peces nadan. Defina dos tipos nuevos: ‘BirdLike‘ y ‘FishLike‘ basados en estos rasgos
+2 Crea un alias de tipo para ‘Bird OR Fish‘ llamado ‘Animal‘
+3 Use ‘instanceof‘ en ‘interrogateAnimal‘ para permitir a los peces nadar y a los pájaros volar
+4 Agregue cualquier anotación de tipo faltante, siendo lo más explı́cito posible
+*/
