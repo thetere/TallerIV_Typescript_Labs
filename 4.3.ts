4e90c6852cf47c81daeda90321ee2f5fddf68a53
thetere <tereseta@gmail.com>
2022-06-01 02:47:33 -0300

    +4.3.ts

--- 4.3.ts
+++ 4.3.ts
@@ -1,7 +1,9 @@
-const numbers = [1, 2, 3, [44, 55], 6, [77, 88], 9, 10];
+const numbers: (number | [number, number])[] = [1, 2, 3, [44, 55], 6, [77, 88], 9, 10];
 
-function flatten(array) {
-    const flattened = [];
+const text: (string | [string, string])[] = ['aaa', 'bbb', ['ccc', 'ddd'], 'eee', ['fff', 'ggg'], 'hhh', 'iii'];
+
+function flatten<Type>(array: (Type | [Type, Type])[]): Type[] {
+    const flattened: Type[] = [];
 
     for (const element of array) {
         if (Array.isArray(element)) {
@@ -16,9 +18,15 @@ function flatten(array) {
     return flattened;
 }
 
-const flattenedNumbers = flatten(numbers);
+const flattenedNumbers: number[] = flatten<number>(numbers);
 
+console.log('[Ejercicio 4.3]', numbers);
 console.log('[Ejercicio 4.3]', flattenedNumbers);
+
+const flattenedText: string[] = flatten<string>(text);
+
+console.log('[Ejercicio 4.3]', text);
+console.log('[Ejercicio 4.3]', flattenedText);
 
 /*
 1 Añadir anotaciones de tipo (‘any‘ excluido)
